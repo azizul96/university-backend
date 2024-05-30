ff83c36b5bd98c81d5bb460dbc621af3040c9169
Azizul Islam <alifalamin42@gmail.com>
2024-05-30 14:36:53 +0600

    university-backend project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["**/node_modules/", ".dist/"],
+
+    languageOptions: { globals: globals.browser },
+
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+    // globals: {
+    //   process: "readonly",
+    // },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
