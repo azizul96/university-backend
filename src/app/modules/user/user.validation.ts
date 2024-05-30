3f4c55bde56348f117b5866c5358a14619d06db0
Azizul Islam <alifalamin42@gmail.com>
2024-05-30 14:58:39 +0600

    update user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -7,10 +7,6 @@ const userValidationSchema = z.object({
     })
     .max(20, { message: 'Password can not be more than 20 characters' })
     .optional(),
-  needsPasswordChange: z.boolean().optional().default(true),
-  role: z.enum(['student', 'faculty', 'admin']),
-  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-  isDeleted: z.boolean().optional().default(false),
 });
 
 export const UserValidation = {
